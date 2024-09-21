db5b45df9c6a692f1ad450317af7d3d6f26832f7
Israr Ahmed <israrahmedpk1999@gmail.com>
2024-09-21 11:09:13 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+
+
+app.get('/welcome', (req, res) => {
+        res.send("Hello World");
+   });
+
+app.listen(8000, () => {
+    console.log("Server is running on port 8000");
+});
